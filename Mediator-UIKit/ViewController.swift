fc4d22929fa00231fd8983d3091e9a56e5516e8f
Mikiya Abe <struuuuggle@gmail.com>
2020-03-29 18:37:28 +0900

    Initial Commit

---
+++ Mediator-UIKit/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Mediator-UIKit
+//
+//  Created by Mikiya Abe on 2020/03/29.
+//  Copyright © 2020 Mikiya Abe. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
